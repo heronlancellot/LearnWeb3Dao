25a05c194c56124a88aa5f9569291773d850c331
heronlancellot <heronlancellot@id.uff.br>
2022-12-01 16:06:31 -0300

    Hardhat configuration & Contract already Deployed initial configuration

--- Sophomore/build-an-NFT-Collection/scripts/deploy.js
+++ Sophomore/build-an-NFT-Collection/scripts/deploy.js
@@ -1,14 +1,25 @@
 const {ethers} = require("hardhat");
+require("dotenv").config({path: ".env"});
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
+
 
 async function main() {
 
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+
+  const metadataURL = METADATA_URL;
   const CryptoDevs = await hre.ethers.getContractFactory("CryptoDevs");
-  const DeployedCryptoDevs = await CryptoDevs.deploy();
+  const DeployedCryptoDevs = await CryptoDevs.deploy(
+    metadataURL,
+    whitelistContract
+  );
 
   await DeployedCryptoDevs.deployed();
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
