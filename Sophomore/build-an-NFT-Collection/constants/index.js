23d80bb15869a84dc03611d6d516721a2199b433
heronlancellot <heronlancellot@id.uff.br>
2022-12-02 20:21:49 -0300

    Folder Constants & index.js with the whitelist_address_contract

---
+++ Sophomore/build-an-NFT-Collection/constants/index.js
@@ -0,0 +1,7 @@
+
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x13Ee7dAE93c5758edf8DBAD8842D308B626f14eE";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
