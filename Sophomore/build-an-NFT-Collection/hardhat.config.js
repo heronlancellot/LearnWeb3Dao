b03317afd814fce87f6506f38fe3b7a8cd068d36
heronlancellot <heronlancellot@id.uff.br>
2022-12-02 20:24:16 -0300

    Correction coment

--- Sophomore/build-an-NFT-Collection/hardhat.config.js
+++ Sophomore/build-an-NFT-Collection/hardhat.config.js
@@ -4,11 +4,10 @@ require("dotenv").config({ path: ".env" });
 const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
-  networks:{
-    goerli:{
+  networks: {
+    goerli: {
       url: QUICKNODE_HTTP_URL,
       accounts: [PRIVATE_KEY],
     },
