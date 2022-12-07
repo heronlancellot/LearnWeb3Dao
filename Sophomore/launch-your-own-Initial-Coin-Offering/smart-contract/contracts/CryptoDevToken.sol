659fa712648332a4de2675f00ec72eab51719387
heronlancellot <heronlancellot@id.uff.br>
2022-12-07 18:07:55 -0300

    CryptoDev contracts

---
+++ Sophomore/launch-your-own-Initial-Coin-Offering/smart-contract/contracts/CryptoDevToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ICryptoDevs.sol";
+
+contract CryptoDevToken is ERC20, Ownable {
+    
+    ICryptoDevs CryptoDevsNFT;
+    constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
+        CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
+    }
+}
