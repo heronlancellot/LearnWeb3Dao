217aba741bf0c4aac72c47a6d32b42b6f265b83e
heronlancellot <heronlancellot@id.uff.br>
2022-12-08 14:14:45 -0300

    Crypto

--- Sophomore/launch-your-own-Initial-Coin-Offering/smart-contract/contracts/CryptoDevToken.sol
+++ Sophomore/launch-your-own-Initial-Coin-Offering/smart-contract/contracts/CryptoDevToken.sol
@@ -6,9 +6,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ICryptoDevs.sol";
 
 contract CryptoDevToken is ERC20, Ownable {
-    
+    // Price of one Crypto Dev token
+    uint256 public constant tokenPrice = 0.001 ether;
+    uint256 public constant tokensPerNFT = 10 * 10**18;
+    // the max total supply is 10000 for Crypto Dev Tokens
+    uint256 public constant maxTotalSupply = 10000 * 10**18;
+
     ICryptoDevs CryptoDevsNFT;
     constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
         CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
     }
+
 }
+
