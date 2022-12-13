6f6b47c2fcda29271a5d4f211619074df99dc69b
heronlancellot <heronlancellot@id.uff.br>
2022-12-13 17:36:43 -0300

    Configuration to Verify Contract Hardhat

--- Sophomore/launch-your-own-Initial-Coin-Offering/smart-contract/hardhat.config.js
+++ Sophomore/launch-your-own-Initial-Coin-Offering/smart-contract/hardhat.config.js
@@ -3,6 +3,7 @@ require("dotenv").config({ path: ".env" });
 
 const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.8.4",
@@ -12,4 +13,7 @@ module.exports = {
       accounts: [PRIVATE_KEY],
     },
   },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
