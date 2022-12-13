b9d2b9177dc6a5b64b9f6a96169b6063d6b20421
heronlancellot <heronlancellot@id.uff.br>
2022-12-13 13:57:38 -0300

    Add Smart Contract

---
+++ Sophomore/launch-your-own-Initial-Coin-Offering/smart-contract/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x98e211f4c324b8bab6476ad0145d8794cdf22361";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
--- Sophomore/launch-your-own-Initial-Coin-Offering/smart-contract/contracts/CryptoDevToken.sol
+++ Sophomore/launch-your-own-Initial-Coin-Offering/smart-contract/contracts/CryptoDevToken.sol
@@ -1,25 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "./ICryptoDevs.sol";
-
-contract CryptoDevToken is ERC20, Ownable {
-    // Price of one Crypto Dev token
-    uint256 public constant tokenPrice = 0.001 ether;
-    uint256 public constant tokensPerNFT = 10 * 10**18;
-    // the max total supply is 10000 for Crypto Dev Tokens
-    uint256 public constant maxTotalSupply = 10000 * 10**18;
-
-    ICryptoDevs CryptoDevsNFT;
-    constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
-        CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
-    }
-
-}
-
-// SPDX-License-Identifier: MIT
   pragma solidity ^0.8.0;
 
   import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
